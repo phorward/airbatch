215a1964e9a95e922600bb7a2bad85d61b034445
Jan Max Meyer <jmm@phorward.de>
2018-07-06 15:28:15 +0200

    HTML interface and improvements

--- airbatch.py
+++ airbatch.py
@@ -160,139 +160,147 @@ class Activity():
 		return txt
 
 	def clone(self, link = None):
-		return Activity(aircraft = self.aircraft, pilot = self.pilot, copilot=self.copilot, link = link)
+		return Activity(aircraft = self.aircraft, pilot = self.pilot, copilot=self.copilot,
+		                    ltakeoff = self.ltakeoff, ltouchdown=self.ltouchdown,
+		                        takeoff = self.touchdown, link = link)
 
+def parse(txt):
+	print(txt)
+	results = []
 
-txt = """
-MK pille
-1029 1035 1218 YX meier, horst schmu  rhkm süm
-MK pille 1035 +7 YL puddy +30 rhmk rhmk
-D-KYYA meier, horst  ruhm   1145 1213   edlw rhmk
-DMRMK meier sta 1150 1230 süm hegenscheid
-"""
+	presetLauncher = None
+	presetDate = None
+	presetAircraft = None
+	presetPilot = None
+	presetCopilot = None
 
-#txt = "MK YL puddy 1035 +10"
-
-print(txt)
-
-presetLauncher = None
-presetDate = None
-presetAircraft = None
-presetPilot = None
-presetCopilot = None
-
-idx = 0
-for s in txt.split("\n"):
-	s = s.strip()
-	if not s:
-		continue
-
-	unknown = []
-	clarify = []
-
-	activities = []
-	current = None
-
-	while s:
-		res = None
-
-		for r in [tr, dr, pr, ar, lr]:
-			res = r.recognize(s)
-			if res:
-				break
-
-		#print(res)
-
-		if res is None:
-			res = R.recognize(s)
-			s = s[res.count:]
-			unknown.append(res)
+	idx = 0
+	for s in txt.split("\n"):
+		s = s.strip()
+		if not s:
 			continue
 
-		if isinstance(res.obj, Aircraft):
-			if not current:
-				current = Activity(res.obj)
+		unknown = []
+		clarify = []
+
+		activities = []
+		current = None
+
+		while s:
+			res = None
+
+			for r in [tr, dr, pr, ar, lr]:
+				res = r.recognize(s)
+				if res:
+					break
+
+			#print(res)
+
+			if res is None:
+				res = R.recognize(s)
+				s = s[res.count:]
+				unknown.append(res)
+				continue
+
+			if isinstance(res.obj, Aircraft):
+				if not current:
+					current = Activity(res.obj)
+				else:
+					current = Activity(res.obj, link=current)
+
+				activities.append(current)
+
+				for cres in clarify[:]:
+					if current.set(cres.obj):
+						clarify.remove(cres)
+
+			elif (current and not current.set(res.obj)) or not current:
+				clarify.append(res)
+
+			#print("%s = %s" % (res.token, res.obj))
+			s = s[res.count:]
+
+		#print(len(activities), clarify)
+
+		if len(activities) == 0 and clarify:
+			if presetAircraft:
+				current = presetAircraft.clone()
+
+				for cres in clarify[:]:
+					print(cres.obj)
+					if current.set(cres.obj):
+						clarify.remove(cres)
+
+				current.complete()
+				activities.append(current)
 			else:
-				current = Activity(res.obj, link=current)
+				first = True
+				for c in clarify[:]:
+					if isinstance(c.obj, Pilot):
+						if first:
+							first = False
+							presetCopilot = None
+							presetPilot = None
 
-			activities.append(current)
+						if not presetPilot:
+							presetPilot = c.obj
+							clarify.remove(c)
+						elif not presetCopilot:
+							presetCopilot = c.obj
+							clarify.remove(c)
 
-			for cres in clarify[:]:
-				if current.set(cres.obj):
-					clarify.remove(cres)
+		if len(activities) == 1:
+			launch = activities[0]
 
-		elif (current and not current.set(res.obj)) or not current:
-			clarify.append(res)
+			if not launch.complete():
+				if launch.aircraft.launcher:
+					presetAircraft = presetLauncher = launch
+				else:
+					presetAircraft = launch
 
-		#print("%s = %s" % (res.token, res.obj))
-		s = s[res.count:]
-
-	if len(activities) == 0 and clarify:
-		if presetAircraft:
-			current = presetAircraft.clone()
-			for cres in clarify[:]:
-				if current.set(cres.obj):
-					clarify.remove(cres)
-
-			activities.append(current)
-		else:
-			first = True
-			for c in clarify[:]:
-				if isinstance(c.obj, Pilot):
-					if first:
-						first = False
-						presetCopilot = None
-						presetPilot = None
-
-					if not presetPilot:
-						presetPilot = c.obj
-						clarify.remove(c)
-					elif not presetCopilot:
-						presetCopilot = c.obj
-						clarify.remove(c)
-
-	if len(activities) == 1:
-		launch = activities[0]
-
-		if not launch.complete():
-			if launch.aircraft.launcher:
-				presetLauncher = launch
 			else:
-				presetAircraft = launch
-		else:
-			if launch.aircraft.kind == "glider" and not launch.aircraft.selfstart:
-				if presetLauncher:
-					llaunch = presetLauncher.clone(launch)
-					activities.insert(0, llaunch)
-					llaunch.complete()
+				if launch.aircraft.kind == "glider" and not launch.aircraft.selfstart:
+					if presetLauncher:
+						llaunch = presetLauncher.clone(launch)
+						activities.insert(0, llaunch)
+						llaunch.complete()
 
-					for cres in clarify[:]:
-						if isinstance(cres.obj, datetime.datetime) and cres.obj >= launch.touchdown:
-							launch.touchdown = cres.obj
-							clarify.remove(cres)
-							launch.complete()
+						for cres in clarify[:]:
+							if isinstance(cres.obj, datetime.datetime) and cres.obj >= launch.touchdown:
+								launch.touchdown = cres.obj
+								clarify.remove(cres)
+								launch.complete()
 
+					else:
+						print("--- %d ---" % idx)
+						print(launch)
+						results.append(launch)
+						idx += 1
+
+						presetAircraft = launch
 				else:
 					print("--- %d ---" % idx)
 					print(launch)
+					results.append(launch)
 					idx += 1
-			else:
-				print("--- %d ---" % idx)
+
+					presetAircraft = launch
+
+		# No else!
+		if len(activities) > 1:
+			if len(activities) > 2:
+				print("Too many launches per row, please spicify only two aircraft in total per row")
+
+			print("--- %d ---" % idx)
+			idx += 1
+			for launch in activities:
+				launch.complete()
 				print(launch)
-				idx += 1
+				results.append(launch)
 
-	# No else!
-	if len(activities) > 1:
-		if len(activities) > 2:
-			print("Too many launches per row, please spicify only two aircraft in total per row")
+		unknown.extend(clarify)
 
-		print("--- %d ---" % idx)
-		idx += 1
-		for launch in activities:
-			launch.complete()
-			print(launch)
+		if unknown:
+			print("Unknown:", [(str(res.obj) if res.obj else res.token) for res in unknown])
 
-	unknown.extend(clarify)
-
-	if unknown:
-		print("Unknown:", [(res.obj or res.token) for res in unknown])
+	return results
